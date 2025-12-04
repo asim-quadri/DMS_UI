639887e98ea45246a354ba19fb19fc102521c2b5
sandeepkumarsiripurapu <sandeep.kumar@ikonostechnologies.com>
2025-12-04 15:08:20 +0530

    updt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 export class AppConfig {
-  // public readonly BaseUrl = 'http://74.208.221.20/services';
-  //  public readonly ServiceUrl='http://74.208.221.20/services/api';
+  public readonly BaseUrl = 'http://74.208.221.20/services';
+   public readonly ServiceUrl='http://74.208.221.20/services/api';
 
   //  public readonly BaseUrl = 'http://74.208.221.20/uatcomplianceapi';
   //  public readonly ServiceUrl='http://74.208.221.20/uatcomplianceapi/api';
 
-   public readonly BaseUrl = 'https://localhost:44314';
-   public readonly ServiceUrl = 'https://localhost:44314/api';
+  //  public readonly BaseUrl = 'https://localhost:44314';
+  //  public readonly ServiceUrl = 'https://localhost:44314/api';
 }
