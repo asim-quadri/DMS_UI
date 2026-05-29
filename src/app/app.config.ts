d31dcfbcb6e7b965091067e519fac79374e35e6c
sandeep kumar <sandeep.kumar@ikonostechnologies.com>
2026-05-29 19:21:27 +0530

    updt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 export class AppConfig {
-  public readonly BaseUrl = 'https://15.235.162.64:4543';
-   public readonly ServiceUrl='https://15.235.162.64:4543/api';
-
-  //  public readonly BaseUrl = 'https://localhost:44315';
-  //  public readonly ServiceUrl = 'https://localhost:44315/api';
+  //uat
+  public readonly BaseUrl = 'http://15.235.162.64:4543';
+   public readonly ServiceUrl='http://15.235.162.64:4543/api';
+//dev
+  // public readonly BaseUrl = 'http://15.235.162.64:4443';
+  //  public readonly ServiceUrl='http://15.235.162.64:4543/api';
 }
