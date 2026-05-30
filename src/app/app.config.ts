a0f3998aa5a972cccaaefb2c7ccedf8a3078affd
babu shaik <sandeep.kumar@ikonostechnologies.com>
2026-05-30 15:10:10 +0530

    updt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 export class AppConfig {
   //uat
-  public readonly BaseUrl = 'http://15.235.162.64:4543';
-   public readonly ServiceUrl='http://15.235.162.64:4543/api';
+  public readonly BaseUrl = ' https://ikonostechnologies.in:4543';
+   public readonly ServiceUrl=' https://ikonostechnologies.in:4543/api';
 //dev
-  // public readonly BaseUrl = 'http://15.235.162.64:4443';
-  //  public readonly ServiceUrl='http://15.235.162.64:4543/api';
+  // public readonly BaseUrl = 'https://ikonostechnologies.in:4443';
+  //  public readonly ServiceUrl='https://ikonostechnologies.in:4443/api';
 }
